d8b04ecf35c8a3bbb188660bb9de8aa6d960d9b0
Ritik <ritik.k.1404@gmail.com>
2024-09-16 20:15:46 +0530

    Initial Setup

---
+++ People/Resources/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  People
+//
+//  Created by Ritik Karma on 16/09/24.
+//
+import SwiftUI
+import Foundation
+
+enum Theme {
+    //use static let if not working
+  static let background = Color("background")
+   static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+    
+}
