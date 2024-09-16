1715d57e7ca55df38cc7f6e2c607ac03d54e40c9
Ritik <ritik.k.1404@gmail.com>
2024-09-16 12:03:53 +0530

    Initial Commit

---
+++ People/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  People
+//
+//  Created by Ritik Karma on 16/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Ritik Karma on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
